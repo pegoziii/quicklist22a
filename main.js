e07e99cf86a0852a561421438fef4d2352b5420e
pegozzigabriel <pegozzigabriel@gmail.com>
2025-05-29 01:23:26 +0000

    adicao da funcao setTimeout para adicionar a classe

--- main.js
+++ main.js
@@ -42,14 +42,15 @@ function showItemsList() {
 } 
 
 
-function checkItem(itemName) {
-    const item = items.find((item) => item.name === itemName)
-    item.checked = !item.checked
-    showItemsList()
-  }
-
-  function removeItem(itemName) {
+function removeItem(itemName) {
     const itemIndex = items.findIndex((item) => item.name === itemName)
+    const divWarning = document.querySelector(".warning")
+  
+    divWarning.classList.remove("hide-warning")
+  
+    setTimeout(() => {
+      divWarning.classList.add("hide-warning")
+    }, 4000)
   
     if (itemIndex !== -1) {
       items.splice(itemIndex, 1)
